e3133b3b5869cebe552e63a1ef5d8cff3f55cac5
ReynaldoHendson <80369346+reynaldo86@users.noreply.github.com>
2023-10-18 19:59:00 -0300

    implement description method controller

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Api Restful Santander Backend 2023
+# Santander Backend 2023
+Projeto API RestFul Java para Santander backend 2023.
 
+## Diagrama de classes
+
+```mermaid
+classDiagram
+  class Pessoa {
+    +id: int
+    +nome: string
+    +sobrenome: string
+    +cpf: string
+    +email: string
+    +telefone: string
+    -endereco: Endereco
+  }
+
+  class Endereco {
+    +cep: string
+    +logradouro: string
+    +complemento: string
+    +bairro: string
+    +localidade: string
+    +uf: string
+  }
+
+  Pessoa "1..*" *-- "1..1" Endereco
+```
--- src/main/java/br/com/api/controller/ClienteController.java
+++ src/main/java/br/com/api/controller/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.api.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,25 +25,35 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("clientes")
+@Tag(name = "Users Controller", description = "RESTful API for managing customer.")
 public class ClienteController {
 
 	@Autowired
 	private ClienteService clienteService;
 
 	@GetMapping
+	@Operation(summary = "Get all customers", description = "Retrieve a list of all registered customers")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Operation successful")
+	})
 	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
 		return ResponseEntity.status(HttpStatus.OK).body(clienteService.buscarTodos());
 	}
 
 	@GetMapping("/{id}/cliente")
+	@Operation(summary = "Get a customer by ID", description = "Retrieve a specific customer based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Operation successful"),
+			@ApiResponse(responseCode = "404", description = "User not found")
+	})
 	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
 		return ResponseEntity.status(HttpStatus.OK).body(clienteService.buscarPorId(id));
 	}
 
 	@PostMapping
-	@Operation(summary = "Create a new user", description = "Create a new user and return the created user's data")
+	@Operation(summary = "Create a new customer", description = "Create a new user and return the created customers data")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "201", description = "User created successfully")})
+			@ApiResponse(responseCode = "201", description = "Customer created successfully")})
 	public ResponseEntity<Cliente> inserir(@RequestBody @Valid Cliente cliente) {
 		clienteService.inserir(cliente);
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
@@ -53,12 +64,23 @@ public class ClienteController {
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "Update a customer", description = "Update the data of an existing customer based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Customer updated successfully"),
+			@ApiResponse(responseCode = "404", description = "Customer not found"),
+			@ApiResponse(responseCode = "422", description = "Invalid customer data provided")
+	})
 	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
 		clienteService.atualizar(id, cliente);
 		return ResponseEntity.ok(cliente);
 	}
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "Delete a customer", description = "Delete an existing customer based on its ID")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "204", description = "Customer deleted successfully"),
+			@ApiResponse(responseCode = "404", description = "Customer not found")
+	})
 	public ResponseEntity<Void> deletar(@PathVariable Long id) {
 		clienteService.deletar(id);
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
